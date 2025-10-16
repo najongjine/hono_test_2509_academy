dc6ef12e970935fbe535c7d4f504f09250d14f58
najongjine <najongjin3@hotmail.com>
2025-10-16 11:25:03 +0900

    .

--- src/router/board_router.ts
+++ src/router/board_router.ts
@@ -1,17 +1,33 @@
 import { Hono } from "hono";
 import { AppDataSource } from "../data-source1";
+import { TBoard } from "../entities/TBoard";
 
 const router = new Hono();
 
-router.get("/test1", async (c) => {
+router.get("/", async (c) => {
   let result: { success: boolean; data: any; msg: string } = {
     success: true,
     data: null,
     msg: ``,
   };
   try {
-    let q = c?.req?.query("q");
-    result.data = q;
+    result.data = "board router 잘 작동 합니다";
+    return c.json(result);
+  } catch (error: any) {
+    return c.json(result);
+  }
+});
+
+router.get("/get_memo_list", async (c) => {
+  let result: { success: boolean; data: any; msg: string } = {
+    success: true,
+    data: null,
+    msg: ``,
+  };
+  try {
+    const boardRepo = AppDataSource.getRepository(TBoard);
+    const memo = boardRepo.find();
+    result.data = memo;
     return c.json(result);
   } catch (error: any) {
     return c.json(result);
