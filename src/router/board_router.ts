86f3f11140f9166d8fbdf6e1fa19151c8a15529c
najongjine <najongjin3@hotmail.com>
2025-10-16 11:50:03 +0900

    .

--- src/router/board_router.ts
+++ src/router/board_router.ts
@@ -26,8 +26,32 @@ router.get("/get_memo_list", async (c) => {
   };
   try {
     const boardRepo = AppDataSource.getRepository(TBoard);
-    const memo = boardRepo.find();
+    const memo = await boardRepo.find();
     result.data = memo;
+    return c.json(result);
+  } catch (error: any) {
+    return c.json(result);
+  }
+});
+
+router.post("/test1", async (c) => {
+  let result: { success: boolean; data: any; msg: string } = {
+    success: true,
+    data: null,
+    msg: ``,
+  };
+  try {
+    const body = await c?.req?.parseBody();
+    let title = String(body["title"]);
+    let content = String(body["content"]);
+
+    /* t_board 에 데이터 저장하기 */
+    const boardRepo = AppDataSource.getRepository(TBoard);
+    let newBoard = new TBoard();
+    newBoard.title = title;
+    newBoard.content = content;
+    newBoard = await boardRepo.save(newBoard);
+    result.data = newBoard;
     return c.json(result);
   } catch (error: any) {
     return c.json(result);
